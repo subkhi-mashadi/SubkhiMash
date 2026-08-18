94dc2e9dcad725ca26899eca5d6baf398a727b4d
subki mashadi <subkhi@konnco.com>
2026-08-18 10:16:12 +0700

    meta name

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="8maWVCsEgqucDu2K0ulOh7FC-05mpcJoKSj6p8IWEqw" />
 
         @php
             $seoProfile = \App\Models\Profile::current();
